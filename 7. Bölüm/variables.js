49cfcfd22bea6f562e93c455a076ea690a30bb0b
nobles <denizopak123@gmail.com>
2022-05-08 21:25:41 +0300

    if-else

---
+++ 7. Bölüm/variables.js
@@ -0,0 +1,33 @@
+let isim = "Deniz"
+console.log(isim)
+
+isim = "Mehmet"
+console.log(isim)
+
+let yas = 10
+let bolme = 10
+
+yas = yas + 5
+yas += 5
+bolme %= 5
+
+console.log("Yeni Yaşı:", yas)
+console.log("Yeni Yaşı: " + yas)
+
+let gelir = 5
+let gider = 10
+
+gelir++
+gider--
+
+console.log(gelir)
+console.log(gider)
+
+const yemek = "Hamburger"
+const icecek = "Kola"
+
+console.log(`Ben yemek olarak ${yemek} istiyorum. İçecek olarak da ${icecek} istiyorum.`)
+
+let degisken = "metin satırı"
+
+console.log(typeof degisken)
