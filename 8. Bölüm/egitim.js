8e602e0e4ad5c2d5f94e66b085d1af9d89fe40b1
nobles <denizopak123@gmail.com>
2022-05-13 22:06:20 +0300

    Functions

---
+++ 8. Bölüm/egitim.js
@@ -0,0 +1,20 @@
+console.log(5)
+console.log("selam")  // seviye kodu
+/*
+bu
+bir
+metin
+satırıdır.
+*/
+
+console.log(/*Seviye*/ 10)
+
+console.log(2) // number
+console.log("selam") // string
+console.log(true) // boolean
+
+console.log("11" + 5)
+
+console.log("Merhaba" + " " + "Dünya")
+
+console.log("Kodlama Öğreniyorum".length)
